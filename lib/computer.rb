62b209eb70ec5cedfae5387aa4e923e7ecee3287
Nate Giesing <nategiesing@Nates-MacBook-Pro.local>
2017-05-25 09:22:50 -0600

    eval commit

--- lib/computer.rb
+++ lib/computer.rb
@@ -10,22 +10,73 @@ attr_reader :grid
               c:  ["C", " ", " ", " ", " "],
               d:  ["D", " ", " ", " ", " "]
             }
+            # require "pry"; binding.pry
+  end
+
+  def ship_one
+    # require "pry"; binding.pry
+    key = row_picker
+    index = column_picker
+    grid_writer(key, index)
+    second_placement_validator(key, index)
   end
 
   def ship_two
-    # get a column from column picker
-    # get a row from row picker
-    # change the grid plot point to $ for the corresponding column and row
-    # ask direction_picker to pick the next plot point (watch out for if statement)
-      # make it pick until the hash value is " ", if so, change hash position to $
+    key = row_picker
+    index = column_picker
+    valid = grid_validator(key, index)
+    if valid
+      grid_writer(key, index)
+    else
+      ship_two
+    end
+  end
+
+
+
+  def grid_writer(key, index)
+    grid[key][index] = "$"
+  end
+
+  def grid_validator(row, column)
+    if ["a", "b", "c", "d"].include?(row)
+      if ["1", "2", "3", "4"].include?(column)
+        true
+    else
+      invalid_message
+      end
+    end
+  end
+
+  def second_placement_validator(key, index)
+    possible_position = []
+    if key != :a
+      row = (key.to_s.ord-1).chr.to_sym
+      possible_position << [row, index]
+    end
+    if index != 4
+      column = index +1
+      possible_position << [key, column]
+    end
+    if key != :d
+      row = (key.to_s.ord+1).chr.to_sym
+      possible_position << [row, index]
+    end
+    if index != 1
+      column = index - 1
+      possible_position << [key, column]
+    end
+    # require "pry"; binding.pry
+    second_position = possible_position.sample
+    grid_writer(second_position[0], second_position[1])
   end
 
   def column_picker
-    # ["1", "2", "3", "4"].random
+    # ["1", "2", "3", "4"].rand
     rand(1..4)
   end
 
   def row_picker
-    ["a", "b", "c", "d"].sample
+    [:a, :b, :c, :d].sample
   end
 end
--- test/computer_test.rb
+++ test/computer_test.rb
@@ -15,7 +15,7 @@ class ComputerTest < Minitest::Test
   def test_it_picks_row
     computer = Computer.new
     actual = computer.row_picker
-    expected = ["a", "b", "c", "d"].include?(actual)
+    expected = [:a, :b, :c, :d].include?(actual)
 
     assert expected
   end
@@ -34,4 +34,57 @@ class ComputerTest < Minitest::Test
 
     assert_equal expected, actual
   end
+
+  def test_cant_place_on_invalid_board_area
+    computer = Computer.new
+    row = "e"
+    column = "5"
+    refute computer.grid_validator(row, column)
+  end
+
+  def test_can_only_place_on_valid_board_area
+    computer = Computer.new
+    row = "a"
+    column = "3"
+    assert computer.grid_validator(row, column)
+  end
+
+  def test_places_the_first_placement
+    computer = Computer.new
+    row = :b
+    column = 3
+    computer.grid_writer(row, column)
+    assert_equal "$", computer.grid[:b][3]
+  end
+
+   def test_if_it_places_right_location_for_second_placement
+    computer = Computer.new
+    row = :b
+    column = 3
+    computer.grid_writer(row, column)
+    # row = :c
+    # column = 4
+    computer.second_placement_validator(row, column)
+    expected = [computer.grid[:c][3], computer.grid[:a][3], computer.grid[:b][2],computer.grid[:b][4]].include?("$")
+
+    assert expected
+  end
+
+  # def test_second_ship_places_valid
+  #   computer = Computer.new
+  #   computer.ship_one
+  #   row = :c
+  #   column = 4
+  #   computer.ship_two
+  #   assert_equal computer.grid.values
+  # end
+
+
+
+
+
+  # def test_for_valid_spot_on_second_ship
+  #   computer = Computr.new
+  #   row =
+  # end
 end
